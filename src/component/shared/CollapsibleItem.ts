9bad8b16bd9ade548d0469b31922f10fe7597197
dilrong <dilrong@dilrong.com>
2022-04-05 21:28:16 +0900

    feat: collapsibleItem title color

--- src/component/shared/CollapsibleItem.ts
+++ src/component/shared/CollapsibleItem.ts
@@ -1,22 +1,29 @@
 import { DomNode, el } from "@hanul/skynode";
 
 export default class CollapsibleItem extends DomNode {
+    private title: DomNode;
     private content: DomNode;
     constructor(title: string, desc: string) {
         super(".collapsible-item");
         this.append(
             el("section", { "data-aos": "fade-up" },
-                el("button.collapsible", title,
+                this.title = el("button.collapsible", title,
                     {
                         click: () => {
                             if (this.content.domElement.style.display === "block") {
                                 this.content.style({
                                     display: "none",
-                                })
+                                });
+                                this.title.style({
+                                    backgroundColor: "none",
+                                });
                             } else {
                                 this.content.style({
                                     display: "block",
-                                })
+                                });
+                                this.title.style({
+                                    backgroundColor: "#00445A",
+                                });
                             }
                         }
                     }),
