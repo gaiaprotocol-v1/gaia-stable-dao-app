db6194ffaacf7a02cd1f90256f7b9145861b93b9
dilrong <dilrong@dilrong.com>
2022-05-09 15:50:37 +0900

    feat: portfolio item rate

--- src/component/PortfolioItem.ts
+++ src/component/PortfolioItem.ts
@@ -12,6 +12,7 @@ export default class PortfolioItem extends DomNode {
     private desc4Display: DomNode;
     private desc5Display: DomNode;
     private priceDisplay: DomNode;
+    private rateDisplay: DomNode;
 
     private claimButton: DomNode;
 
@@ -19,6 +20,7 @@ export default class PortfolioItem extends DomNode {
         id: number,
         symbol: string,
         name: string,
+        startPrice: number,
         desc2: string,
         desc3: string,
         desc4?: string,
@@ -33,8 +35,11 @@ export default class PortfolioItem extends DomNode {
             el(".content",
                 this.desc1Display = el("p", `${msg("PORTFOLIO_ITEM_DESC1")} ${symbol.toUpperCase()} (${name.toUpperCase()})`),
                 this.desc2Display = el("p", `${msg("PORTFOLIO_ITEM_DESC2")} ${desc2}`),
-                el("p", `시작가: ₩3,403.33`),
-                this.priceDisplay = el("p", "현재가: "),
+                el("p", `시작가: ₩${startPrice}`),
+                el(".price-container",
+                    this.priceDisplay = el("p", "현재가: "),
+                    this.rateDisplay = el("p.rate", ""),
+                ),
                 this.desc3Display = el("p", `${msg("PORTFOLIO_ITEM_DESC3")} ${desc3}`),
                 // this.desc4Display = el("p", `${msg("PORTFOLIO_ITEM_DESC4")}: ${desc4}`),
                 // this.desc5Display = el("p", `${msg("PORTFOLIO_ITEM_DESC5")}: ${desc5}`),
@@ -42,14 +47,25 @@ export default class PortfolioItem extends DomNode {
             // this.claimButton = el("button", msg("PORTFOLIO_BUTTON")),
         );
 
-        this.getPrice(symbol);
+        this.getPrice(symbol, startPrice);
     }
 
-    async getPrice(symbol: string) {
+    async getPrice(symbol: string, price: number) {
         const result = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=krw&ids=${symbol}`);
         const data = await result.json();
+        const rate = (data[0].current_price / price) * 100 - 100;
         this.priceDisplay.empty();
         this.priceDisplay.appendText(`현재가: ₩${CommonUtil.numberWithCommas(data[0].current_price)}`);
+        this.rateDisplay.appendText(`(${rate.toFixed(2)}%)`);
+        if (rate > 0) {
+            this.rateDisplay.style({
+                color: "#D9000F"
+            })
+        } else {
+            this.rateDisplay.style({
+                color: "#1738C1"
+            })
+        }
     }
 
     public delete() {
